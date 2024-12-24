f27ed390fd8d5b408ca94d9d5ddf0f4cdbe80e9c
Adam K <aking@gumgum.com>
2024-12-24 10:54:06 -0800

    added more questions

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,24 +54,35 @@ questions = dict((i, qa) for i, qa in enumerate([
 				["[Մմ]եսրոպ [Մմ]աշտոց|mesrr?o[pb]", "405"],
 				match_all = True),
 			QA(4,
+				"According to the eldest son of Denethor, the Steward of Gondor, what may one not do to enter the volcanic land of the east?",
+				["walk|stroll"])
+			QA(5,
 				"What are the three kinds of cheese used in the making of the world's best boreg?",
 				["mozz?arella", "feta", "farmer|californi|fresh"],
 				match_all = True),
-			QA(5,
+			QA(6,
 				"What job did Sona Tanteeg have in Lebanon?",
 				["architect"]),
-			QA(6,
+			QA(7,
 				"To what restaurant did Zaroug demand her father take her before/after Sunday barbecues as a child? (multiple possible answers)",
-				["panda express", "taco llama", r"in(\-n\-| and | ?& ?)out"])
+				["panda express", "taco llama", r"in(\-n\-| and | ?& ?)out"]),
+			QA(8,
+				"Who is Syra's best friend at school?",
+				["maya"]),
+			QA(9,
+				"Who is the most beautiful, hardest working and most loving mother, wife and daughter who also played bass in a high school band called \"17+\"?",
+				["zaroug"]),
 			]))
 
 if 'q_idx' not in st.session_state:
 	st.session_state['q_idx'] = 0
 
-
+st.header('White Elephant 2024 Quiz')
+st.markdown("""8 out of 10 required to gain the password to unlock this year's most interesting present.
+	""")
 guesses = {}
 for q_idx, qa in sorted(questions.items(), key = lambda x : x[0]):
-	st.write(f'{q_idx + 1}: {qa.question}')
+	st.write(f'{q_idx + 1}. {qa.question}')
 	guesses[q_idx] = st.text_input(label = '', key = f'q_{q_idx}')
 
 if st.button('Submit'):
